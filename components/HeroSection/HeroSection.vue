88a142a5ac619a5ab5c60aaa4aad5b9cd2bbab51
Fran Iglesias <franiglesias@mac.com>
2025-05-13 22:19:20 +0200

    Front Page

--- components/HeroSection/HeroSection.vue
+++ components/HeroSection/HeroSection.vue
@@ -1,37 +1,48 @@
 <template>
-  <section class="hero-section">
-      <div>
-        <h2 class="claim">
-          <span class="text-gradient">The #GastroTech event in Galicia</span> 🐙
-        </h2>
+  <section
+      class="hero-section">
+    <div>
+      <h2 class="claim">
+        <span
+            class="text-gradient">The #GastroTech event in Galicia</span>
+        🐙
+      </h2>
 
-        <h3 class="date">
-          Vigo, 18, 19 and 20 september 2025
-        </h3>
-      </div>
-<!--
-      <div>
-        <a class="btn" href="https://entradas.pulpocon.es" target="_blank">
-          🎟 ¡Entradas disponibles! 🎟
-        </a>
-      </div>
-      <div>
-        <a class="btn" href="https://bit.ly/pulpoCon24SponsorES" target="_blank">
-          Dossier Patrocinadores
-        </a>
-        <a class="btn" href="https://bit.ly/pulpoCon24SponsorEN" target="_blank">
-          Sponsors Deck
-        </a>
-      </div>
-      -->
-      <div class="claims">
-        <a class="btn" href="#summary">
-          <i class="fas fa-arrow-down"></i> Más información | More information <i class="fas fa-arrow-down"></i>
-        </a>
-      </div>
+      <h3 class="date">
+        Vigo, September, 18, 19 & 20, 2025
+      </h3>
+    </div>
+    <!--
+          <div>
+            <a class="btn" href="https://entradas.pulpocon.es" target="_blank">
+              🎟 Tickets Available! 🎟
+            </a>
+          </div>
+                -->
+    <div>
+      <a class="btn"
+         href="https://docs.google.com/presentation/d/1pRGA1fH5ULCLFxAamWO92UqcpQc1iEQjOMT2zw6kgpU/edit?usp=sharing"
+         target="_blank">
+        Sponsors
+        Deck
+      </a>
+    </div>
+
+    <div
+        class="claims">
+      <a class="btn"
+         href="#summary">
+        <i class="fas fa-arrow-down"></i>
+        More information
+        <i class="fas fa-arrow-down"></i>
+      </a>
+    </div>
   </section>
 </template>
 
-<style src="./HeroSection.scss"></style>
-<script setup lang="ts">
+<style
+    src="./HeroSection.scss"></style>
+<script
+    setup
+    lang="ts">
 </script>
