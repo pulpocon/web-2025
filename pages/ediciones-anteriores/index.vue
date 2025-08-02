fd5aa420ca77fd744823e4e5eecdf0d6a5e13c2c
Fran Iglesias <franiglesias@mac.com>
2025-08-02 19:05:44 +0200

    2024 video

--- pages/ediciones-anteriores/index.vue
+++ pages/ediciones-anteriores/index.vue
@@ -5,13 +5,23 @@
         <header class="page-simple__header">
           <h1 class="page-simple__title">Previous Editions</h1>
         </header>
-
         <div class="page-simple__content">
           <h2 class="heading-2">2024</h2>
-          <a class="btn" href="https://2024.pulpocon.es">Visita 2024</a>
-
+          <p>2024 was our first time ever with international speakers. Another milestone that we are going to complete in 2025. </p>
+          <div class="responsive-embed-wrapper mb-5">
+            <iframe
+                allow="accelerometer; autoplay; loop; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+                frameborder="0"
+                height="315"
+                src="https://www.youtube-nocookie.com/embed/lR1T0sajiFM"
+                width="560">
+            </iframe>
+          </div>
+          <a class="btn" href="https://2024.pulpocon.es">Visit 2024</a>
           <h2 class="heading-2">2023</h2>
-          <p>The 2023 edition brought together 250 unique attendees over three days from all parts of Spain, confirming PulpoCon as the great #GastroTech hub worldwide.</p>
+          <p>The 2023 edition brought together 250 unique attendees over three days from all parts of Spain, confirming
+            PulpoCon as the great #GastroTech hub worldwide.</p>
 
           <div class="responsive-embed-wrapper mb-5">
             <iframe
@@ -26,7 +36,8 @@
           <a class="btn" href="https://2023.pulpocon.es">Visit 2023</a>
 
           <h2 class="heading-2">2022</h2>
-          <p>The 2022 edition brought together 280 unique attendees from all parts of Spain, with the main areas of origin being Madrid, Barcelona, Valencia, Seville, Asturias, Vigo, and A Coruña.</p>
+          <p>The 2022 edition brought together 280 unique attendees from all parts of Spain, with the main areas of
+            origin being Madrid, Barcelona, Valencia, Seville, Asturias, Vigo, and A Coruña.</p>
 
           <div class="responsive-embed-wrapper mb-5">
             <iframe
@@ -41,7 +52,8 @@
           <a class="btn" href="https://2022.pulpocon.es">Visit 2022</a>
 
           <h2 class="heading-2">2019</h2>
-          <p>The 2019 edition was the first edition of <strong>PulpoCon</strong>, and it was a great success with 200 attendees from various parts of Spain, mainly Vigo and Barcelona.</p>
+          <p>The 2019 edition was the first edition of <strong>PulpoCon</strong>, and it was a great success with 200
+            attendees from various parts of Spain, mainly Vigo and Barcelona.</p>
 
           <div class="responsive-embed-wrapper mb-5">
             <iframe
